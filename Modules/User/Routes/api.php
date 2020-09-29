1079cb7cd0b3272efcd8c8876451b09de01a0dc8
Gabriel Petrescu <p.gaby92@gmail.com>
2020-09-29 13:53:19 +0300

    Remove Middleware for login check status and check status in AuthController

--- Modules/User/Routes/api.php
+++ Modules/User/Routes/api.php
@@ -15,12 +15,12 @@ use Illuminate\Http\Request;
 
 Route::middleware('jwt:api')->resource('/users', UserController::class)->except(['edit', 'create']);
 Route::prefix('users')->group(function () {
+    Route::post('/login', 'AuthController@login');
     Route::post('/register', 'UserController@store');
     Route::post('/verify', 'UserController@verify');
     Route::post('/{user}/forgot', 'UserController@forgotPassword');
     Route::post('/password', 'UserController@changePassword');
     Route::middleware('jwt:api')->post('/{user}/role', 'UserController@assignRoleToUser');
-    Route::middleware('checkstatus')->post('/login', 'AuthController@login');
     Route::middleware('jwt:api')->post('/{user}/approve', 'UserController@approve');
     Route::middleware('jwt:api')->post('/{user}/confirmation', 'UserController@resendConfirmation');
     Route::middleware('jwt:api')->post('/registerAdmin', 'UserController@createAccount');
