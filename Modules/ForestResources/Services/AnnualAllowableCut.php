4f574be0816516049d6fd8d5b8c49ab7b947f9ce
Gabriel Petrescu <p.gaby92@gmail.com>
2020-10-15 17:52:26 +0300

    Service and Request validator

---
+++ Modules/ForestResources/Services/AnnualAllowableCut.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Modules\ForestResources\Services;
+
+
+use App\Services\PageResults;
+use Modules\ForestResources\Entities\AnnualAllowableCut as AnnualAllowableCutEntity;
+
+class AnnualAllowableCut extends PageResults
+{
+
+    public function getPaginator()
+    {
+
+        $this->query = AnnualAllowableCutEntity::ofSort($this->getSortCriteria());
+
+        return $this->setFilters(['Name', 'Geometry'])->getResults();
+    }
+
+
+}
