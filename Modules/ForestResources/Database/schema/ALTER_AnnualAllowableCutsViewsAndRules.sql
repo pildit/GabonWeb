380186e306b863a1b06ac0056c0d9c09731607d4
Constantin Dinu <ctin.dinu@gmail.com>
2020-10-22 10:49:17 +0300

    fix sql for constituent permits

--- Modules/ForestResources/Database/schema/ALTER_AnnualAllowableCutsViewsAndRules.sql
+++ Modules/ForestResources/Database/schema/ALTER_AnnualAllowableCutsViewsAndRules.sql
@@ -23,34 +23,58 @@ FROM "ForestResources"."AnnualAllowableCutsTable" as mut;
 -----------
 
 CREATE RULE "AnnualAllowableCuts_instead_of_insert" AS
-    ON INSERT TO "ForestResources"."AnnualAllowableCuts" DO INSTEAD  INSERT INTO "ForestResources"."AnnualAllowableCutsTable" ("Id", "ResourceType", "Name", "AacId", "ManagementUnit", "ManagementPlan", "Geometry", "CreatedAt", "UpdatedAt")
-  VALUES (nextval('"ForestResources"."SEQ_BaseResources"'::regclass), ( SELECT rt."Id"
+    ON INSERT TO "ForestResources"."AnnualAllowableCuts"
+    DO INSTEAD
+    INSERT INTO "ForestResources"."AnnualAllowableCutsTable"
+        (
+         "Id",
+         "ResourceType",
+         "Name",
+         "AacId",
+         "ManagementUnit",
+         "ManagementPlan",
+         "Geometry",
+         "CreatedAt",
+         "UpdatedAt"
+         )
+  VALUES (
+          nextval('"ForestResources"."SEQ_BaseResources"'::regclass),
+          ( SELECT rt."Id"
            FROM "ForestResources"."ResourceTypes" rt
           WHERE (rt."Name" = 'Annual Allowable Cut'::text)
-         LIMIT 1), new."Name", new."AacId", new."ManagementUnit", new."ManagementPlan", new."Geometry", new."CreatedAt", new."UpdatedAt")
-  RETURNING "ForestResources"."AnnualAllowableCutsTable"."Id",
-    "ForestResources"."AnnualAllowableCutsTable"."Name",
-    "ForestResources"."AnnualAllowableCutsTable"."AacId",
-    "ForestResources"."AnnualAllowableCutsTable"."ManagementUnit",
-    "ForestResources"."AnnualAllowableCutsTable"."ManagementPlan",
-    "ForestResources"."AnnualAllowableCutsTable"."Geometry",
-    "ForestResources"."AnnualAllowableCutsTable"."CreatedAt",
-    "ForestResources"."AnnualAllowableCutsTable"."UpdatedAt",
-    "ForestResources"."AnnualAllowableCutsTable"."DeletedAt";
-
+         LIMIT 1),
+          new."Name",
+          new."AacId",
+          new."ManagementUnit",
+          new."ManagementPlan",
+          new."Geometry",
+          new."CreatedAt",
+          new."UpdatedAt"
+          )
+  RETURNING
+      "Id",
+      "Name",
+      "AacId",
+      "ManagementUnit",
+      "ManagementPlan",
+      "Geometry",
+      "CreatedAt",
+      "UpdatedAt",
+      "DeletedAt";
 
 CREATE RULE "AnnualAllowableCuts_instead_of_update" AS
     ON UPDATE TO "ForestResources"."AnnualAllowableCuts" DO INSTEAD  UPDATE "ForestResources"."AnnualAllowableCutsTable" SET "Name" = new."Name", "AacId" = new."AacId", "ManagementUnit" = new."ManagementUnit", "ManagementPlan" = new."ManagementPlan", "Geometry" = new."Geometry", "UpdatedAt" = new."UpdatedAt", "DeletedAt" = new."DeletedAt"
   WHERE ("ForestResources"."AnnualAllowableCutsTable"."Id" = old."Id")
-  RETURNING "ForestResources"."AnnualAllowableCutsTable"."Id",
-    "ForestResources"."AnnualAllowableCutsTable"."Name",
-    "ForestResources"."AnnualAllowableCutsTable"."AacId",
-    "ForestResources"."AnnualAllowableCutsTable"."ManagementUnit",
-    "ForestResources"."AnnualAllowableCutsTable"."ManagementPlan",
-    "ForestResources"."AnnualAllowableCutsTable"."Geometry",
-    "ForestResources"."AnnualAllowableCutsTable"."CreatedAt",
-    "ForestResources"."AnnualAllowableCutsTable"."UpdatedAt",
-    "ForestResources"."AnnualAllowableCutsTable"."DeletedAt";
+  RETURNING
+      "Id",
+      "Name",
+      "AacId",
+      "ManagementUnit",
+      "ManagementPlan",
+      "Geometry",
+      "CreatedAt",
+      "UpdatedAt",
+      "DeletedAt";
 
 CREATE RULE "AnnualAllowableCuts_instead_of_delete" AS
     ON DELETE TO "ForestResources"."AnnualAllowableCuts" DO INSTEAD  DELETE FROM  "ForestResources"."AnnualAllowableCutsTable"
