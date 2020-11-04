cca92b8afc54540662afa347f9486e2251d6e10c
Gabriel Petrescu <p.gaby92@gmail.com>
2020-11-04 18:20:57 +0200

    sql file beautify

--- Modules/Transport/Database/schema/rename_Transportation_create_Permits_tables.sql
+++ Modules/Transport/Database/schema/rename_Transportation_create_Permits_tables.sql
@@ -1,42 +1,42 @@
--- alter schema transportation rename to "Transportation";
+alter schema transportation rename to "Transportation";
 
 create table "Transportation"."PermitsTable"
 (
-    "Id"                      serial not null
+    "Id"                    serial                              not null
         primary key,
-    "Recdate"                 timestamp default now(),
-    "Obsdate"                 date,
-    "Appuser"                 varchar,
-    "Lat"                 real,
-    "Lon"                     real,
-    "GpsAccu"                integer,
-    "PermitNo"               varchar,
-    "HarvestName"            varchar,
-    "ClientName"             varchar,
-    "ConcessionName"         varchar,
-    "TransportComp"          varchar,
-    "LicensePlate"           varchar,
-    "Destination"             varchar,
-    "ManagementUnit"         varchar,
-    "OperationalUnit"        varchar,
+    "Recdate"               timestamp default now(),
+    "Obsdate"               date,
+    "Appuser"               varchar,
+    "Lat"                   real,
+    "Lon"                   real,
+    "GpsAccu"               integer,
+    "PermitNo"              varchar,
+    "HarvestName"           varchar,
+    "ClientName"            varchar,
+    "ConcessionName"        varchar,
+    "TransportComp"         varchar,
+    "LicensePlate"          varchar,
+    "Destination"           varchar,
+    "ManagementUnit"        varchar,
+    "OperationalUnit"       varchar,
     "AnnualOperationalUnit" varchar,
-    "Note"                    varchar,
-    "TheGeom"                geometry,
+    "Note"                  varchar,
+    "TheGeom"               geometry,
     "PoductType"            varchar,
-    "PermitStatus"           varchar,
-    "VerifiedBy"             varchar,
-    "TransportBy"            varchar,
-    "GeneratedBy"            varchar,
-    "ScanLat"                real,
-    "ScanLon"                real,
+    "PermitStatus"          varchar,
+    "VerifiedBy"            varchar,
+    "TransportBy"           varchar,
+    "GeneratedBy"           varchar,
+    "ScanLat"               real,
+    "ScanLon"               real,
     "ScanGpsAccu"           integer,
-    "Photos"                  varchar,
-    "Farmer"                  varchar,
-    "FirstProvince"          varchar,
-    "MobileId"               varchar,
-    "CreatedAt" timestamp default current_timestamp not null,
-    "UpdatedAt" timestamp default current_timestamp not null,
-    "DeletedAt" timestamp
+    "Photos"                varchar,
+    "Farmer"                varchar,
+    "FirstProvince"         varchar,
+    "MobileId"              varchar,
+    "CreatedAt"             timestamp default current_timestamp not null,
+    "UpdatedAt"             timestamp default current_timestamp not null,
+    "DeletedAt"             timestamp
 );
 
 
@@ -51,37 +51,41 @@ create sequence if not exists "Transportation"."SEQ_PermitsTable"
 
 -- update_timestamp function
 create or replace function update_timestamp()
-    returns trigger as $$
+    returns trigger as
+$$
 begin
     new."UpdatedAt" = now();
     return new;
 end ;
 $$ language 'plpgsql';
 
-create trigger user_timestamp before insert or update on "Transportation"."PermitsTable"
-    for each row execute procedure update_timestamp();
+create trigger user_timestamp
+    before insert or update
+    on "Transportation"."PermitsTable"
+    for each row
+execute procedure update_timestamp();
 
 
 create table "Transportation"."PermitItemsTable"
 (
-    "Id"           serial not null
+    "Id"          serial                              not null
         primary key,
-    "Permit"    bigint,
+    "Permit"      bigint,
     "TrunkNumber" varchar,
     "LotNumber"   varchar,
-    "Species"      varchar,
-    "Diam1"        varchar,
-    "Diam2"        varchar,
+    "Species"     varchar,
+    "Diam1"       varchar,
+    "Diam2"       varchar,
     "DiamAvg"     varchar,
-    "Length"       varchar,
-    "Volume"       varchar,
-    "Width"        varchar,
-    "Height"       varchar,
+    "Length"      varchar,
+    "Volume"      varchar,
+    "Width"       varchar,
+    "Height"      varchar,
     "MobileId"    varchar,
-    "Appuser"      varchar,
-    "CreatedAt" timestamp default current_timestamp not null,
-    "UpdatedAt" timestamp default current_timestamp not null,
-    "DeletedAt" timestamp
+    "Appuser"     varchar,
+    "CreatedAt"   timestamp default current_timestamp not null,
+    "UpdatedAt"   timestamp default current_timestamp not null,
+    "DeletedAt"   timestamp
 );
 
 -- Foregin Keys
@@ -91,7 +95,7 @@ alter table "Transportation"."PermitItemsTable"
             (
              "Permit"
                 )
-            references "Transportation"."PermitsTable"("Id")
+            references "Transportation"."PermitsTable" ("Id")
 ;
 
 
@@ -106,12 +110,16 @@ create sequence if not exists "Transportation"."SEQ_PermitItemsTable"
 
 -- update_timestamp function
 create or replace function update_timestamp()
-    returns trigger as $$
+    returns trigger as
+$$
 begin
     new."UpdatedAt" = now();
     return new;
 end ;
 $$ language 'plpgsql';
 
-create trigger user_timestamp before insert or update on "Transportation"."PermitItemsTable"
-    for each row execute procedure update_timestamp();
+create trigger user_timestamp
+    before insert or update
+    on "Transportation"."PermitItemsTable"
+    for each row
+execute procedure update_timestamp();
