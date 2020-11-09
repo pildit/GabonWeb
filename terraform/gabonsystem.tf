813d6d4a5508dd102831770f9a046914da316cd3
TD <tdelrue@eia-global.org>
2020-11-09 09:36:23 -0500

    terraform: adding HTTPS Load Balancer routing based on Host Header

--- terraform/gabonsystem.tf
+++ terraform/gabonsystem.tf
@@ -192,6 +192,52 @@ resource "aws_lb" "albProduction" {
   }
 }
 
+resource "aws_lb_target_group" "lbtgHttpsProductionWebsite" {
+  name     = "lbtgHttpsProductionWebsite"
+  port     = 443
+  protocol = "HTTPS"
+  vpc_id   = aws_vpc.vpcGabonSystem.id
+
+  target_type = "instance"
+}
+
+resource "aws_lb_target_group_attachment" "lbtgaHttpsToProdInstance" {
+  target_group_arn = aws_lb_target_group.lbtgHttpsProductionWebsite.arn
+  target_id        = aws_instance.iProductionInstance.id
+}
+
+resource "aws_lb_listener" "alblHttpsFrontEnd" {
+  load_balancer_arn = aws_lb.albProduction.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  #   ssl_policy        = "ELBSecurityPolicy-2016-08"
+  #   certificate_arn   = "arn:aws:iam::187416307283:server-certificate/test_cert_rab3wuqwgja25ct3n4jdj2tzu4"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.lbtgHttpsProductionWebsite.arn
+  }
+}
+
+resource "aws_lb_listener_rule" "albProductionRule" {
+  listener_arn = aws_lb_listener.alblHttpsFrontEnd.arn
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.lbtgHttpsProductionWebsite.arn
+  }
+
+  condition {
+    host_header {
+      values = [
+        "globalorigin.org",
+        "*.globalorigin.org"
+      ]
+    }
+  }
+}
+
 output "vpc-id" {
   value = aws_vpc.vpcGabonSystem.id
 }
