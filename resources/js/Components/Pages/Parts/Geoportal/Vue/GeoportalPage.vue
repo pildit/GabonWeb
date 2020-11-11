46e5b64cd03e1166e6614576a455bb38538835b8
ICristi <iliecristian2009@gmail.com>
2020-11-11 19:02:55 +0200

    Added basic sidebar in Geoportal feature.

--- resources/js/Components/Pages/Parts/Geoportal/Vue/GeoportalPage.vue
+++ resources/js/Components/Pages/Parts/Geoportal/Vue/GeoportalPage.vue
@@ -6,6 +6,18 @@
       ref="map-root"
       v-bind:style="{ height: window.height + 'px' }"
     ></div>
+
+    <div class="options"></div>
+
+    <!-- Content of the menu -->
+    <div id="menu" v-bind:style="{ height: window.height + 'px' }">
+      <h1>Menu</h1>
+      <p style="border-bottom: 1px solid #999">
+        <i>ol.control.Overlay</i> can be used to display a menu or information
+        on the top of the map.
+      </p>
+      <div class="data"></div>
+    </div>
   </div>
 </template>
 
@@ -18,6 +30,7 @@ import OSM from "ol/source/OSM";
 // importing the OpenLayers stylesheet is required for having
 // good looking buttons!
 import "ol/ol.css";
+import "ol-ext/style/defaultStyle";
 
 import olExtAnimatedCluster from "ol-ext/layer/AnimatedCluster";
 import olCluster from "ol/source/Cluster";
@@ -27,6 +40,8 @@ import olGeomPoint from "ol/geom/Point";
 import olGeomPolygon from "ol/geom/Polygon";
 import olView from "ol/View";
 import olExtSelectCluster from "ol-ext/interaction/SelectCluster";
+import olExtOverlay from "ol-ext/control/Overlay";
+import olExtToggle from "ol-ext/control/Toggle";
 import * as olCoordinate from "ol/coordinate/";
 import * as olStyle from "ol/style";
 // import * as olStyle from "ol-ext/style";
@@ -114,6 +129,25 @@ export default {
         constrainResolution: true,
       }),
     });
+
+    // Overlay
+    var menu = new olExtOverlay({
+      closeBox: true,
+      className: "menu",
+      content: $("#menu").get(0),
+    });
+    map.addControl(menu);
+
+    // A toggle control to show/hide the menu
+    var t = new olExtToggle({
+      html: '<i class="fa fa-bars" ></i>',
+      className: "menu",
+      title: "Menu",
+      onToggle: function () {
+        menu.toggle();
+      },
+    });
+    map.addControl(t);
 
     // Style for the clusters
     var styleCache = {};
@@ -239,3 +273,74 @@ export default {
   },
 };
 </script>
+
+<style>
+.ol-overlay.menu {
+  width: 30%;
+  background: #fff;
+  color: #333;
+  box-shadow: 0px 0px 5px #000;
+  padding: 0.5em;
+  -webkit-transition: all 0.25s;
+  transition: all 0.25s;
+}
+/* style the close box */
+.ol-overlay.menu .ol-closebox {
+  color: #369;
+  left: 1em;
+  top: 0.5em;
+}
+.ol-overlay.menu .ol-closebox:before {
+  content: "\f0c9";
+  font-family: FontAwesome;
+}
+#menu {
+  padding-top: 1.5em;
+  font-size: 0.9em;
+}
+/* menu button */
+.ol-control.menu {
+  top: 0.5em;
+  left: 0.5em;
+}
+.ol-control.menu i {
+  color: #fff;
+  transition: all .5s ease-in-out;
+}
+.ol-zoom {
+  left: auto;
+  right: 0.5em;
+}
+.ol-rotate {
+  right: 3em;
+}
+.ol-touch .ol-rotate {
+  right: 3.5em;
+}
+/**/
+.ol-overlay img {
+  max-width: 90%;
+}
+.ol-overlay.slide-up {
+  transform: translateY(100%);
+  -webkit-transform: translateY(100%);
+}
+.ol-overlay.slide-down {
+  -webkit-transform: translateY(-100%);
+  transform: translateY(-100%);
+}
+.ol-overlay.slide-left
+{	-webkit-transform: translateX(-100%);
+  transform: translateX(-100%);
+}
+.ol-overlay.slide-right {
+  -webkit-transform: translateX(100%);
+  transform: translateX(100%);
+}
+.data,
+.data p {
+  margin: 0;
+  text-align: center;
+  font-size: 0.9em;
+}
+</style>
