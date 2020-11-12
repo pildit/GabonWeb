66cc43f9c111fe0f0a07ad2913fdbc6bb4e5400a
Tudor Danes <tudor.danes@gmail.com>
2020-11-12 19:04:26 +0200

    Use Trait UserEmailAttribute

--- Modules/ForestResources/Entities/Quality.php
+++ Modules/ForestResources/Entities/Quality.php
@@ -4,17 +4,19 @@ namespace Modules\ForestResources\Entities;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Traits\Sortable;
+use App\Traits\UserEmailAttribute;
 
 class Quality extends Model
 {
-    use Sortable;
+    use Sortable, UserEmailAttribute;
 
     protected $fillable = ['Value', 'Description'];
 
-    protected $table = "Taxonomy.Quality";
+    protected $table = "ForestResources.InventoryQualitiesTable";
 
     const CREATED_AT = "CreatedAt";
     const UPDATED_AT = "UpdatedAt";
+    public $timestamps = true;
 
     protected $dateFormat = 'Y-m-d H:i:s.u';
 
