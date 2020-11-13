3300b3f8980b3b087f3a456b8639d0b70a4ab21a
Constantin Dinu <ctin.dinu@gmail.com>
2020-11-13 16:02:19 +0200

    trnasform coord into WGS84; flip the coordinates

--- Modules/ForestResources/Services/AnnualAllowableCutInventory.php
+++ Modules/ForestResources/Services/AnnualAllowableCutInventory.php
@@ -20,8 +20,10 @@ class AnnualAllowableCutInventory extends PageResults
     public function getVectors($bbox)
     {
         $srid = config('forestresources.srid');
-        $collection = AnnualAllowableCutInventoryEntity::select(['Id', DB::raw('public.ST_AsGeoJSON("Geometry") as geom')])
-            ->whereRaw("public.ST_Intersects(public.st_setsrid(\"Geometry\", {$srid}), public.st_setsrid(public.ST_MakeEnvelope({$bbox}), {$srid}))")->get();
+        $geomCol = DB::raw('public.ST_AsGeoJSON(public.st_flipcoordinates(public.st_transform("Geometry",4256))) as geom');
+        $whereIntersects = "public.ST_Intersects(public.st_setsrid(\"Geometry\", {$srid}), public.st_setsrid(public.ST_MakeEnvelope({$bbox}), {$srid}))";
+        $collection = AnnualAllowableCutInventoryEntity::select(['Id', $geomCol])
+            ->whereRaw($whereIntersects)->get();
 
         return $collection->map(function ($item) {
             return [
