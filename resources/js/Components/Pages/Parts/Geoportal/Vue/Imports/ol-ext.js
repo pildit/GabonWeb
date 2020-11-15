8d1070c37791cbff2a907854d744d79d9fad0320
Ilie <iliecristian2009@gmail.com>
2020-11-15 18:47:54 +0200

    Added files of exports to reduce the number of imports and to improve the readability of the code

---
+++ resources/js/Components/Pages/Parts/Geoportal/Vue/Imports/ol-ext.js
@@ -0,0 +1,6 @@
+export {default as Overlay} from "ol-ext/control/Overlay"
+export {default as Toggle} from "ol-ext/control/Toggle"
+export {default as AnimatedCluster} from "ol-ext/layer/AnimatedCluster"
+export {default as SelectCluster } from "ol-ext/interaction/SelectCluster"
+export {default as FlowLine } from "ol-ext/style/FlowLine"
+export {default as Composite} from 'ol-ext/filter/Composite'
---
+++ resources/js/Components/Pages/Parts/Geoportal/Vue/Imports/ol.js
@@ -0,0 +1,18 @@
+export { default as View } from "ol/View"
+export { default as Collection } from "ol/Collection";
+export { default as Feature } from "ol/Feature"
+export { default as Cluster } from "ol/source/Cluster"
+export { default as Vector } from "ol/source/Vector"
+export { default as OSM } from "ol/source/OSM"
+export { default as Point } from "ol/geom/Point"
+export { default as Polygon } from "ol/geom/Polygon"
+export { default as LineString } from "ol/geom/LineString";
+export { default as Coordinate } from "ol/coordinate"
+export { default as Style } from "ol/style/Style"
+export { default as Circle } from "ol/style/Circle"
+export { default as Fill } from "ol/style/Fill"
+export { default as Text } from "ol/style/Text"
+export { default as Stroke } from "ol/style/Stroke"
+export { default as VectorImage } from "ol/layer/VectorImage"
+export { default as LayerVector } from "ol/layer/Vector"
+
