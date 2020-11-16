84b3de87d3ce3ac33052fd8d78067050dd1bc875
Tudor Danes <tudor.danes@gmail.com>
2020-11-16 15:54:31 +0200

    Concessions landing page ( use component box-resource) Parcels

---
+++ resources/js/Components/Parcel/Parts/Grid/Vue/ItemForm.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <div class="md-form mb-5">
+      <input type="text" v-model="form.Name" name="Name" class="form-control" v-validate="'required'">
+      <label data-error="wrong" data-success="right" for="name" :class="{'active': form.Name}">{{ translate('Abbreviation') }}</label>
+      <div v-show="errors.has('Name')" class="invalid-feedback">{{ errors.first('Name') }}</div>
+    </div>
+
+
+  </div>
+</template>
+
+<script>
+import Item from "../../../Parcel";
+
+
+export default {
+  data() {
+    return {
+      form : {},
+    }
+  },
+
+  computed: {
+  },
+
+  methods: {
+    save() {
+      this.$validator.validate().then((valid) => {
+        if(valid) {
+          Item.add({
+              Name: this.form.Name
+          }).then((response) => {
+            this.$emit('done');
+          }).catch((error) => {
+            if(error) {
+              this.$setErrorsFromResponse(error.data);
+            }
+          })
+        }
+      });
+    },
+    update() {
+      this.$validator.validate().then((valid) => {
+        if(valid) {
+          Item.update(this.form.Id, {
+              Name: this.form.Name
+            }).then(() => {
+            this.$emit('done');
+          }).catch((error) => {
+            if(error) {
+              this.$setErrorsFromResponse(error.data);
+            }
+          })
+        }
+      })
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Components/Parcel/Parts/Grid/Vue/ItemModal.vue
@@ -0,0 +1,66 @@
+<template>
+  <bmodal ref="itemModal" size="medium" :closed="() => $emit('display', false)">
+    <div slot="title">
+      <h4 class="modal-title w-100 font-weight-bold">{{translate('add_permit_type')}}</h4>
+    </div>
+    <div slot="body">
+      <item-form ref="itemForm" @done="closeModal"></item-form>
+    </div>
+    <div slot="footer">
+      <button @click="submit" class="btn btn-default">Save</button>
+      <button @click="closeModal" class="btn btn-warning">Cancel</button>
+    </div>
+  </bmodal>
+</template>
+
+<script>
+import bmodal from 'components/Common/BootstrapModal.vue';
+import itemForm from "./itemForm";
+import {mapGetters} from 'vuex';
+
+export default {
+  model: {
+      prop: 'state',
+    event: 'display'
+  },
+
+  props: ['state', 'typeProp', 'rowProp'],
+
+  components: {bmodal, itemForm},
+
+  computed: {
+    ...mapGetters('permitTypes', ['permitTypes'])
+  },
+
+  methods: {
+    submit() {
+      if(this.typeProp == 'create') {
+        this.$refs.itemForm.save();
+      }else{
+        this.$refs.itemForm.update();
+      }
+    },
+    closeModal() {
+      this.$refs.itemModal.close();
+      this.$emit('done');
+      Vent.$emit('grid-refresh');
+    }
+  },
+
+  watch: {
+    state(val) {
+      if(!val) return;
+      if(this.typeProp != 'create') {
+        this.$refs.itemForm.form = this.rowProp;
+      }else{
+        this.$refs.itemForm.form = {};
+      }
+      this.$refs.itemForm.errors.clear();
+      this.$refs.itemModal.open();
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
