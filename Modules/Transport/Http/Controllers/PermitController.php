5c33cbd00ddd4ca71edd1b286e9d6c56a77eab1e
Casîr Nicolae <nicolae.casir@gmail.com>
2020-11-25 04:33:43 +0200

    Uncommented middlewares from __construct(){}

--- Modules/Transport/Http/Controllers/PermitController.php
+++ Modules/Transport/Http/Controllers/PermitController.php
@@ -21,13 +21,13 @@ class PermitController extends Controller
 
     public function __construct()
     {
-        //$this->middleware('permission:permit.view')->only('index', 'show');
+        $this->middleware('permission:permit.view')->only('index', 'show');
 
-        //$this->middleware('permission:permit.add')->only('store');
+        $this->middleware('permission:permit.add')->only('store');
 
-        //$this->middleware('permission:permit.edit')->only('update');
+        $this->middleware('permission:permit.edit')->only('update');
 
-        //$this->middleware('permission:permit.scan_qr_code')->only('update');
+        $this->middleware('permission:permit.scan_qr_code')->only('update');
 
 //        $this->middleware('role:admin')->only('delete');
 
