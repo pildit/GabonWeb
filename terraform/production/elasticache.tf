7530210de22c7aa466466192416ee6e8cec02941
TD <tdelrue@eia-global.org>
2021-01-05 10:35:35 -0500

    Terraform: Placing elasticache in a subnet group

--- terraform/production/elasticache.tf
+++ terraform/production/elasticache.tf
@@ -1,19 +1,29 @@
+resource "aws_elasticache_subnet_group" "elasticachesubnetgrpProduction" {
+  name = "elasticachesubnetgrpProduction"
+  subnet_ids = [
+    aws_subnet.subnProductionPublic_1a.id
+    , aws_subnet.subnProductionPublic_1b.id
+  ]
+}
+
 resource "aws_elasticache_cluster" "cacheGabonWebFE" {
   cluster_id           = "cache-gabon-webfe"
   engine               = "redis"
   node_type            = "cache.r5.large"
   num_cache_nodes      = 1
   parameter_group_name = "default.redis6.x"
-  engine_version       = "6.x"
+  # engine_version       = "6.x" # 6.0.5
   port                 = 6379
 
-  apply_immediately    = true
+  subnet_group_name = aws_elasticache_subnet_group.elasticachesubnetgrpProduction.name
 
-#   lifecycle {
-#     ignore_changes = [engine_version]
-#   }
+  apply_immediately = true
 
-  tags= {
+  #   lifecycle {
+  #     ignore_changes = [engine_version]
+  #   }
+
+  tags = {
     Name        = "cacheGabonWebFE"
     Environment = "production"
     Terraform   = "1"
@@ -41,12 +51,12 @@ resource "aws_security_group_rule" "sgrWebFEtoElastiCache" {
   from_port         = aws_elasticache_cluster.cacheGabonWebFE.port
   to_port           = aws_elasticache_cluster.cacheGabonWebFE.port
   protocol          = "tcp"
-  cidr_blocks       = [
-      aws_subnet.subnProductionPublic_1a.cidr_block,
-      aws_subnet.subnProductionPublic_1b.cidr_block
+  cidr_blocks = [
+    aws_subnet.subnProductionPublic_1a.cidr_block,
+    aws_subnet.subnProductionPublic_1b.cidr_block
   ]
 }
 
-output "ElastiCache-cache_nodes"{
-    value= aws_elasticache_cluster.cacheGabonWebFE.cache_nodes
+output "ElastiCache-cache_nodes" {
+  value = aws_elasticache_cluster.cacheGabonWebFE.cache_nodes
 }
