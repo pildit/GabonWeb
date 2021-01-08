d5aabf3b7d81ee00af61c7e31aebb5104d4ae2bf
Gabriel Petrescu <p.gaby92@gmail.com>
2021-01-09 00:11:23 +0200

    new columns for logbook,sitelogbook,permits and small fix for flyway R__ files

--- database/sql/R__Create_Accounts_View.sql
+++ database/sql/R__Create_Accounts_View.sql
@@ -1,5 +1,5 @@
-drop view if exists admin."Accounts";
-create view admin."Accounts" as
+-- drop view if exists admin."Accounts";
+create or replace view admin."Accounts" as
 select acc.id,
        acc.firstname,
        acc.password,
@@ -17,13 +17,13 @@ from admin.accounts acc
 left join "Taxonomy"."CompaniesTable" com on acc.company_id = com."Id";
 
 
-CREATE RULE "accounts_instead_of_delete" AS
+CREATE OR REPLACE RULE "accounts_instead_of_delete" AS
     ON DELETE TO "admin"."Accounts" DO INSTEAD
     DELETE
     FROM "admin"."accounts"
     WHERE "accounts"."id" = old."id";
 
-CREATE RULE "accounts_insted_of_insert" AS
+CREATE OR REPLACE RULE "accounts_insted_of_insert" AS
     ON INSERT TO "admin"."Accounts" DO INSTEAD
     INSERT INTO "admin"."accounts" (id, firstname, password, email, lastname, activationcode, status, employee_type, created_at, updated_at, company_id)
     VALUES (
@@ -54,7 +54,7 @@ CREATE RULE "accounts_insted_of_insert" AS
                 (select com."Name" from "Taxonomy"."CompaniesTable" com where accounts."company_id" = com."Id" limit 1),
                 "accounts"."deleted_at";
 
-CREATE RULE "accounts_instead_of_update" AS
+CREATE OR REPLACE RULE "accounts_instead_of_update" AS
     ON UPDATE TO "admin"."Accounts" DO INSTEAD
     UPDATE "admin"."accounts"
     SET "id"           = new."id",
