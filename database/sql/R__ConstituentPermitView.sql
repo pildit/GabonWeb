7456583bd0ba4411742efec5fca6d3201dee29c8
Constantin Dinu <ctin.dinu@gmail.com>
2021-01-14 08:43:10 +0200

    fix Constituent permit view

--- database/sql/R__ConstituentPermitView.sql
+++ database/sql/R__ConstituentPermitView.sql
@@ -1,21 +1,22 @@
-
+drop view if exists "ForestResources"."ConstituentPermits";
 create or replace view "ForestResources"."ConstituentPermits"
-            ("Id", "User","Concession", "Email", "PermitType", "PermitNumber", "Geometry", "Approved", "CreatedAt", "UpdatedAt",
-             "DeletedAt") as
+            ("Id", "User","Concession", "Email", "PermitType", "PermitNumber", "PermitTypeName",
+                "Geometry", "Approved", "CreatedAt", "UpdatedAt", "DeletedAt") as
 SELECT cpt."Id",
        cpt."User",
        cpt."Concession",
        acc.email AS "Email",
        cpt."PermitType",
        cpt."PermitNumber",
+       ptt."Name" as PermitTypeName,
        cpt."Geometry",
        cpt."Approved",
        cpt."CreatedAt",
        cpt."UpdatedAt",
        cpt."DeletedAt"
-FROM "ForestResources"."ConstituentPermitsTable" cpt,
-     admin.accounts acc
-WHERE cpt."User" = acc.id;
+FROM "ForestResources"."ConstituentPermitsTable" cpt
+         LEFT JOIN admin.accounts acc on cpt."User" = acc.id
+         LEFT JOIN "ForestResources"."PermitTypesTable" ptt on cpt."PermitType" = ptt."Id";
 
 alter table "ForestResources"."ConstituentPermits"
     owner to postgres;
@@ -33,9 +34,23 @@ CREATE or replace RULE "ConstituentPermits_instead_of_insert" AS
                                                                            new."User",new."Concession", new."PermitType",
                                                                            new."PermitNumber", new."Geometry",
                                                                            new."Approved", new."CreatedAt")
-                                                                   RETURNING "ConstituentPermitsTable"."Id", "ConstituentPermitsTable"."User", "ConstituentPermitsTable"."Concession", (SELECT acc.email
-                                                                                                                                                FROM admin.accounts acc
-                                                                                                                                                WHERE "ConstituentPermitsTable"."User" = acc.id) AS email, "ConstituentPermitsTable"."PermitType", "ConstituentPermitsTable"."PermitNumber", "ConstituentPermitsTable"."Geometry", "ConstituentPermitsTable"."Approved", "ConstituentPermitsTable"."CreatedAt", "ConstituentPermitsTable"."UpdatedAt", "ConstituentPermitsTable"."DeletedAt";
+                                                                   RETURNING "ConstituentPermitsTable"."Id",
+                                                                       "ConstituentPermitsTable"."User",
+                                                                       "ConstituentPermitsTable"."Concession",
+                                                                       (SELECT acc.email
+                                                                        FROM admin.accounts acc
+                                                                        WHERE "ConstituentPermitsTable"."User" = acc.id) AS email,
+                                                                       "ConstituentPermitsTable"."PermitType",
+                                                                       "ConstituentPermitsTable"."PermitNumber",
+                                                                       (SELECT ptt."Name"
+                                                                        FROM "ForestResources"."PermitTypesTable" ptt
+                                                                        WHERE "ConstituentPermitsTable"."PermitType" = ptt."Id"
+                                                                        LIMIT 1) as "PermitTypeName",
+                                                                       "ConstituentPermitsTable"."Geometry",
+                                                                       "ConstituentPermitsTable"."Approved",
+                                                                       "ConstituentPermitsTable"."CreatedAt",
+                                                                       "ConstituentPermitsTable"."UpdatedAt",
+                                                                       "ConstituentPermitsTable"."DeletedAt";
 
 CREATE or replace RULE "ConstituentPermits_instead_of_update" AS
     ON UPDATE TO "ForestResources"."ConstituentPermits" DO INSTEAD UPDATE "ForestResources"."ConstituentPermitsTable"
@@ -48,8 +63,20 @@ CREATE or replace RULE "ConstituentPermits_instead_of_update" AS
                                                                        "UpdatedAt"    = new."UpdatedAt",
                                                                        "DeletedAt"    = new."DeletedAt"
                                                                    WHERE "ConstituentPermitsTable"."Id" = old."Id"
-                                                                   RETURNING "ConstituentPermitsTable"."Id", "ConstituentPermitsTable"."User","ConstituentPermitsTable"."Concession", (SELECT acc.email
-                                                                                                                                                FROM admin.accounts acc
-                                                                                                                                                WHERE "ConstituentPermitsTable"."User" = acc.id) AS email, "ConstituentPermitsTable"."PermitType", "ConstituentPermitsTable"."PermitNumber", "ConstituentPermitsTable"."Geometry", "ConstituentPermitsTable"."Approved", "ConstituentPermitsTable"."CreatedAt", "ConstituentPermitsTable"."UpdatedAt", "ConstituentPermitsTable"."DeletedAt";
-
-
+                                                                   RETURNING "ConstituentPermitsTable"."Id",
+                                                                       "ConstituentPermitsTable"."User",
+                                                                       "ConstituentPermitsTable"."Concession",
+                                                                       (SELECT acc.email
+                                                                        FROM admin.accounts acc
+                                                                        WHERE "ConstituentPermitsTable"."User" = acc.id) AS email,
+                                                                       "ConstituentPermitsTable"."PermitType",
+                                                                       "ConstituentPermitsTable"."PermitNumber",
+                                                                       (SELECT ptt."Name"
+                                                                        FROM "ForestResources"."PermitTypesTable" ptt
+                                                                        WHERE "ConstituentPermitsTable"."PermitType" = ptt."Id"
+                                                                        LIMIT 1) as "PermitTypeName",
+                                                                       "ConstituentPermitsTable"."Geometry",
+                                                                       "ConstituentPermitsTable"."Approved",
+                                                                       "ConstituentPermitsTable"."CreatedAt",
+                                                                       "ConstituentPermitsTable"."UpdatedAt",
+                                                                       "ConstituentPermitsTable"."DeletedAt";
